28548af686e0fe1a2aac45c5127801fdaeb10a96
curryguo <2856466740@qq.com>
2023-08-25 13:46:47 +0800

    文件读写

---
+++ file_read_write.cpp
@@ -0,0 +1,156 @@
+//1、文本文件---写
+#include <fstream>
+
+void test01()
+{
+	ofstream ofs;
+	ofs.open("test.txt", ios::out);
+
+	ofs << "姓名：张三" << endl;
+	ofs << "性别：男" << endl;
+	ofs << "年龄：18" << endl;
+
+	ofs.close();
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
+
+
+//2、文本文件---读
+#include <fstream>
+#include <string>
+void test01()
+{
+	ifstream ifs;
+	ifs.open("test.txt", ios::in);
+
+	if (!ifs.is_open())
+	{
+		cout << "文件打开失败" << endl;
+		return;
+	}
+
+	//第一种方式
+	//char buf[1024] = { 0 };
+	//while (ifs >> buf)
+	//{
+	//	cout << buf << endl;
+	//}
+
+	//第二种
+	//char buf[1024] = { 0 };
+	//while (ifs.getline(buf,sizeof(buf)))
+	//{
+	//	cout << buf << endl;
+	//}
+
+	//第三种
+	//string buf;
+	//while (getline(ifs, buf))
+	//{
+	//	cout << buf << endl;
+	//}
+
+	char c;
+	while ((c = ifs.get()) != EOF)
+	{
+		cout << c;
+	}
+
+	ifs.close();
+
+
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
+
+
+
+//3、二进制文件---写
+#include <fstream>
+#include <string>
+
+class Person
+{
+public:
+	char m_Name[64];
+	int m_Age;
+};
+
+//二进制文件  写文件
+void test01()
+{
+	//1、包含头文件
+
+	//2、创建输出流对象
+	ofstream ofs("person.txt", ios::out | ios::binary);
+	
+	//3、打开文件
+	//ofs.open("person.txt", ios::out | ios::binary);
+
+	Person p = {"张三"  , 18};
+
+	//4、写文件
+	ofs.write((const char *)&p, sizeof(p));
+
+	//5、关闭文件
+	ofs.close();
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
+
+
+//4、二进制文件---读
+#include <fstream>
+#include <string>
+
+class Person
+{
+public:
+	char m_Name[64];
+	int m_Age;
+};
+
+void test01()
+{
+	ifstream ifs("person.txt", ios::in | ios::binary);
+	if (!ifs.is_open())
+	{
+		cout << "文件打开失败" << endl;
+	}
+
+	Person p;
+	ifs.read((char *)&p, sizeof(p));
+
+	cout << "姓名： " << p.m_Name << " 年龄： " << p.m_Age << endl;
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
